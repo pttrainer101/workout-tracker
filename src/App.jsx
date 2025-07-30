05e5cf6e36c31ffd246f85503bdd62ba24d9b081
pttrainer101 <TRAINANDGAIN2@icloud.com>
2025-07-29 23:31:11 -0700

    Add React App.jsx component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🏋️ Workout Tracker</h1>
+      <p>Your app is running successfully on Render!</p>
+    </div>
+  );
+}
